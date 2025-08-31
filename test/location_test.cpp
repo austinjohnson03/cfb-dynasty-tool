ba31ea24b0685322f132331258ae1476c3b3c359
austinjohnson03 <amj102998@gmail.com>
2025-08-31 15:53:48 -0400

    updated location test to include comparison test

--- test/location_test.cpp
+++ test/location_test.cpp
@@ -3,50 +3,68 @@
 
 #include <gtest/gtest.h>
 
-TEST(LocationTest, ConstructorWithEnum) {
+class LocationTest : public ::testing::Test {
+  protected:
+    Location l1;
+    Location l2;
+    Location l3;
+    void SetUp() override {
+      l1 = Location("New York", State::NY);
+      l2 = Location("New York", State::NY);
+      l3 = Location("Albany", State::NY);
+    }
+};
+
+TEST_F(LocationTest, operator_test) {
+  EXPECT_TRUE(l1 == l2);
+  EXPECT_TRUE(l1 != l3);
+  EXPECT_TRUE(l2 != l3);
+}
+
+TEST(location_test, ConstructorWithEnum) {
   Location loc("Los Angeles", State::CA);
 
   EXPECT_EQ(loc.get_city(), "Los Angeles");
   EXPECT_EQ(loc.get_state(), State::CA);
 }
 
-TEST(LocationTest, ConstructorWithString) {
+TEST(location_test, ConstructorWithString) {
   Location loc("New York", "NY");
 
   EXPECT_EQ(loc.get_city(), "New York");
   EXPECT_EQ(loc.get_state(), State::NY);
 }
 
-TEST(LocationTest, SettersAndGetters) {
-    Location loc; // Test empty setter
+TEST(location_test, SettersAndGetters) {
+  Location loc; // Test empty setter
 
-    loc.set_city("Chicago");
-    EXPECT_EQ(loc.get_city(), "Chicago");
+  loc.set_city("Chicago");
+  EXPECT_EQ(loc.get_city(), "Chicago");
 
-    loc.set_state(State::IL);
-    EXPECT_EQ(loc.get_state(), State::IL);
+  loc.set_state(State::IL);
+  EXPECT_EQ(loc.get_state(), State::IL);
 
-    loc.set_state("CA");
-    EXPECT_EQ(loc.get_state(), State::CA);
+  loc.set_state("CA");
+  EXPECT_EQ(loc.get_state(), State::CA);
 
-    loc.set_id(42);
-    EXPECT_EQ(loc.get_id(), 42);
+  loc.set_id(42);
+  EXPECT_EQ(loc.get_id(), 42);
 }
 
-TEST(LocationTest, InternationalTest) {
+TEST(location_test, InternationalTest) {
   Location loc("Paris", "INT");
 
   EXPECT_EQ(loc.get_city(), "Paris");
   EXPECT_EQ(loc.get_state(), State::INT);
 }
 
-TEST(LocationTest, TestUnknown) {
+TEST(location_test, TestUnknown) {
   Location loc("Miami", "Gibberish");
 
   EXPECT_EQ(loc.get_state(), State::UNK);
 }
 
-TEST(LocationTest, ToString) {
-    Location loc("Austin", State::TX);
-    EXPECT_EQ(loc.to_string(), "Austin, TX");
+TEST(location_test, ToString) {
+  Location loc("Austin", State::TX);
+  EXPECT_EQ(loc.to_string(), "Austin, TX");
 }
