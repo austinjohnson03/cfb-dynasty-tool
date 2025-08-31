eeaa8b42bc281dc15d2f50b18b110cea0334af19
austinjohnson03 <amj102998@gmail.com>
2025-08-31 15:23:29 -0400

    feat: added comparison operators

--- include/archetype.h
+++ include/archetype.h
@@ -7,10 +7,20 @@ class Archetype {
   public:
     Archetype();
     Archetype(ArchetypeType at);
+    Archetype(std::string at);
     ~Archetype() = default;
 
+    bool operator==(const Archetype& other) const {
+      return archetype_type == other.get_archetype_type();
+    }
+
+    bool operator!=(const Archetype& other) const {
+      return !(*this == other);
+    }
+
     std::string get_name() const;
-    ArchetypeType get_archetype_type();
+    ArchetypeType get_archetype_type() const;
+
 
   private:
     std::string name;
